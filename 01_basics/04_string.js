cde12dff6b41d56040bac0f6cd3ff2d1b8a2c3f3
Chandarmauli <130555993+Chandarmauli@users.noreply.github.com>
2024-02-26 21:23:32 +0530

    Create 04_string.js

---
+++ 01_basics/04_string.js
@@ -0,0 +1,17 @@
+const name = "Chandarmauli"
+const repoCount = 50
+
+// console.log(name + repoCount)
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Suduko')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('d'));
