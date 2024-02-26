207f9c62201965d05da59a17086ce4625df9daea
Chandarmauli <130555993+Chandarmauli@users.noreply.github.com>
2024-02-26 16:37:18 +0000

    math

---
+++ 01_basics/05_number.js
@@ -0,0 +1,22 @@
+const score = 300
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000000
+console.log(hundreds.toLocaleString('en=IN'));
+
+
+//---------------------Maths------------------------------
+
+console.log(Math);
+console.log(Math.abs(-4));
