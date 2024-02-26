e4df473ab6bdc776c728d216f02f003f04e08cfa
Chandarmauli <130555993+Chandarmauli@users.noreply.github.com>
2024-02-26 13:07:59 +0000

    summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,15 @@
+//primitive
+
+//7 types : String, Number, Boolean, null, undefined, Symbol, Bigint
+const score = 100
+const scoreValue = 300.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+
+//reference (non primitive)
+
+//array, objects, Functions
