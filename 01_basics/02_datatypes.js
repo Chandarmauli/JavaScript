54b96804eaa2f32a0dd06a4c042136ca8462abcd
Chandarmauli <130555993+Chandarmauli@users.noreply.github.com>
2024-02-26 07:52:07 +0000

    learn variable concept

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";  //treat all js code as newer version
+
+//alert(3+3)     // we are using node js , not browser
+
+let name = "Chandarmauli"  //String
+let age = 18 //int
+let isLoggedIn = true
+let state; //undefined
+
+
+// number => 2 to power 53
+//bigint
+// string => ""
+// boolean => true /false
+// null  => standalone value
+// undefined =>
+// symbol => unique
+
+//object 
+
+console.log(typeof "Chandarmauli")
