c1490fe545ff06d42391500ef1461eabbdb58b13
Chandarmauli <130555993+Chandarmauli@users.noreply.github.com>
2024-02-26 21:21:49 +0530

    Update dataTypes-summary.js

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -9,7 +9,32 @@ const outSideTemp = null
 let userEmail;
 
 const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber =656098458783455n
 
 //reference (non primitive)
 
 //array, objects, Functions
+
+const heroes = ["shaktiman", "nagraj", "doga"];
+
+let mtObj = {
+    name: "chandarmauli",
+    age: 22
+}
+
+const myFuction = function () {
+    console.log("Hello JS learner");
+}
+
+console.log(typeof myFuction);
+
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+
+// Stack ( Primitive), Heap (Non -Primitive)
+
+let myName = "Stranger"
